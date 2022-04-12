967068ee769d1d4cf3d62cec753087635d87ae0a
Felipe Aranha <felipearanha.c@gmail.com>
2022-04-11 22:12:09 -0300

    alteração na route para middleware

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { response } = require("express")
+const { application } = require("express")
 const express = require("express")
 const { v4: uuidv4 } = require("uuid")
 
@@ -7,6 +7,17 @@ const app = express()
 app.use(express.json())
 
 const customers = []
+
+function verifyIfExistsAccountCPF(request, response, next){
+    const { cpf } = request.headers
+    const customer = customers.find(customer => customer.cpf === cpf)
+    
+    if(!customer){
+        return response.status(400).json({error: "Customer not found"})
+    } 
+    request.customer = customer
+    return next()
+}
 
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body
@@ -29,6 +40,13 @@ app.post("/account", (request, response) => {
     })
 
     return response.status(201).send()
+})
+
+//app.use(verifyIfExistsAccountCPF)
+
+app.get("/statement",verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request
+    return response.json(customer.statement)
 })
 
 app.listen(3333)
